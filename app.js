81f1b1fe83352892a074fd8ebc562559d5df8d38
hayden7913 <hayden321@gmail.com>
2020-06-09 21:54:32 -0700

    switches to slider input

--- app.js
+++ app.js
@@ -4,14 +4,19 @@ const BASE_URL = 'https://dog.ceo/api/breeds/image/random';
 const fetchRandDogs = (numDogs) => axios(`${BASE_URL}/${numDogs}`);
 
 $(async () => {
-  $("#js-dogs-form").submit(async function (event) {
+  $(document).on('input', '#slider', function() {
+    $('#slider_value').html( $(this).val() );
+  });
+
+  $('#js-dogs-form').submit(async function (event) {
     event.preventDefault();
-    const numDogsInputEl = $(".js-num-dog-input");
-    const numDogsInputVal =  numDogsInputEl.val()
+    const numDogsInputVal =  $('#slider').val();
     const {data} = await fetchRandDogs(numDogsInputVal);
     const dogImageUrls = data.message;
     console.log(dogImageUrls)
     });
+
+
 });
 
 
